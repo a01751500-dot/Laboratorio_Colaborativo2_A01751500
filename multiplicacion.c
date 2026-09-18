7555f6ab6604d3435d62ec9033f7b425ea73ef56
Jorge Karl Silva Hernández <jorgekarl@MacBook-Pro-de-Jorge.local>
2026-09-18 17:44:40 -0600

    Agrega función multiplicacion

---
+++ multiplicacion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int multiplicacion(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int a, b;
+
+    printf("Ingresa el primer numero: ");
+    scanf("%d", &a);
+
+    printf("Ingresa el segundo numero: ");
+    scanf("%d", &b);
+
+    printf("Resultado de la multiplicacion: %d\n", multiplicacion(a, b));
+
+    return 0;
+}
+
