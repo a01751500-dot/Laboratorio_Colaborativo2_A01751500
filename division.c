3296718aeee33e2ac720ca193d642e73c07ea921
Jorge Karl Silva Hernández <jorgekarl@MacBook-Pro-de-Jorge.local>
2026-09-18 17:51:17 -0600

    Agrega función division

---
+++ division.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, resultado;
+
+    printf("Ingresa el primer numero: ");
+    scanf("%f", &a);
+
+    printf("Ingresa el segundo numero: ");
+    scanf("%f", &b);
+
+    if (b != 0) {
+        resultado = a / b;
+        printf("La division es: %.2f\n", resultado);
+    } else {
+        printf("No se puede dividir entre cero.\n");
+    }
+
+    return 0;
+}
+
